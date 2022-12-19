d0d656772e5f54da535fd89aff9a43f57f789632
Christian Rackerseder <git@echooff.de>
2022-12-19 20:12:56 +0100

    Solve day 1 part 1 in 2022

---
+++ 2022/Day01/Solution.fsx
@@ -0,0 +1,15 @@
+﻿open System.IO
+
+let inputPath = Path.Combine [| __SOURCE_DIRECTORY__; "input.txt" |]
+let allLines = File.ReadAllText inputPath
+
+let elvesCalories = allLines.Split "\n\n"
+
+let sumCalories (elveCalories: string) =
+    elveCalories.Split "\n"
+    |> Array.map (fun calorie -> calorie |> int)
+    |> Array.sum
+
+let highestTotalCalories = elvesCalories |> Array.map sumCalories |> Array.max
+
+printf "%i" highestTotalCalories
